31c2faa55bd9e62228770b4c94d9322fdfbc96cd
jorisgirardot <joris.girardot@gmail.com>
2014-05-01 11:31:36 +0200

    Project creation

---
+++ auto_mechanic/auto_mechanic/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace auto_mechanic
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
