53ac5e2560f6cd94dc8a12f2fab915fa262a1860
jorisgirardot <joris.girardot@gmail.com>
2014-05-01 11:43:26 +0200

    Add BLL project; backup config files

---
+++ auto_mechanic/auto_mechanic.BLL/model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\Joris GIRARDOT\Documents\auto_mechanic\auto_mechanic\auto_mechanic.BLL\model.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
