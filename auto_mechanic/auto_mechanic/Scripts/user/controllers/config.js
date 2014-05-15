9fbe218f9cd9e56ec457812aee912af1753c2906
jorisgirardot <joris.girardot@gmail.com>
2014-05-15 22:02:53 +0200

    Simulation conf page

---
+++ auto_mechanic/auto_mechanic/Scripts/user/controllers/config.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+angular.module('simulationApp')
+  .controller('ConfigCtrl', function ($scope, Cars, Brands, Franchises) {
+      $scope.cars = Cars.query();
+      $scope.brands = Brands.query();
+      $scope.franchises = Franchises.query();
+
+      $scope.add = function (form) {
+      };
+  });
